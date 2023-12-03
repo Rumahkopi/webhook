a67f934a0ea0c572894d24556dba9b6d0c606f87
GilarYa <gilarwahibul@gmail.com>
2023-12-03 23:01:44 +0700

    pushhh

---
+++ type.go
@@ -0,0 +1,13 @@
+package webhook
+
+type Reply struct {
+	Message string `bson:"messsage"`
+}
+
+var stringreply = []string{
+	"kamu mau ngapain kak?",
+	"genchananaaa genchanaaaayoooo",
+	"jangan begitu dong kak sabar ya",
+	"coba kamu share live lokasi kamu biar aku cek",
+	"gilarnya kayaknya gada deh tunggu aja ya",
+}
